686319cee3d5d29ed9b2a75729418d3232e701da
Aleksander <Aleksanderr7@yandex.ru>
2023-09-18 20:10:13 +0300

    Добавил новый пакет Collection и класс IteratorExample

---
+++ src/main/java/Collection/IteratorExample.java
@@ -0,0 +1,21 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add("Иван");
+        arrayList1.add("Мария");
+        arrayList1.add("Владимир");
+        arrayList1.add("Яна");
+
+        Iterator<String> iterator = arrayList1.iterator();
+        while(iterator.hasNext()){
+            iterator.next();
+
+        }
+        System.out.println(arrayList1);
+    }
+}
